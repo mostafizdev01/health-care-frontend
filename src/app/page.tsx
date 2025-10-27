b296ffe89f91298815bc7d899f4c84b49425b932
Mostafiz <mostafiz4372@gmail.com>
2025-10-27 16:03:58 +0600

    health-care client setup completed

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className=" text-3xl text-center">This is home page</h2>
+      <Button>click me</Button>
+    </div>
+  );
+}
