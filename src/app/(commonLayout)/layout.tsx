b14e9b3adf4bb216d7ae7938baa9f925922a2fc6
Mostafiz <mostafiz4372@gmail.com>
2025-10-27 22:34:48 +0600

    ceated hero section

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -3,13 +3,13 @@ import { Navbar } from "../components/shared/navbar";
 
 function CommonLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className=" flex flex-col min-h-dvh">
       <Navbar />
-      <div className=" flex min-h-dvh">
+      <div className=" flex flex-1 justify-center overflow-hidden">
         {children}
       </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,11 @@
+import { Hero } from "../components/modules/Home/Hero";
 
 
 export default function Home() {
   return (
     <div>
       <main>
-        
+        <Hero />
       </main>
     </div>
   );
